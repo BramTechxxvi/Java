28204fe4439d20b671f7c8b0782db579479b4237
BramTechxxvi <bramtechxxvi@gmail.com>
2024-12-07 11:46:22 +0100

    Add files via upload

---
+++ TaskSix.java
@@ -0,0 +1,33 @@
+public class TaskSix{
+		
+		public static void main(String[] args){
+
+
+
+
+	int counter = 4;
+	int answer1 = 1;
+	for(int number = 1; number <= 5; number++){
+	   answer1*=counter;
+	   System.out.println(answer1);
+}
+
+	int counte = 8;
+	int answer2 = 1;
+	for(int number = 1; number <= 5; number++){
+	   answer2*=counte;
+           System.out.println(answer2);
+	
+	
+}
+
+
+
+
+
+}
+
+
+
+
+}
