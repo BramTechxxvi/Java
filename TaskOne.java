d796b36f97c275ffd4d15168c509eb180d326c31
Ibraheem <bramtechxxvi@gmail.com>
2024-12-07 10:55:22 +0100

    first commit

---
+++ TaskOne.java
@@ -0,0 +1,13 @@
+public class TaskOne{
+	public static void main(String[] args){
+
+
+		for(int number = 1; number <= 10; number++){
+			if(number % 2 == 0);
+			System.out.println(number);
+
+
+}
+}
+
+}
