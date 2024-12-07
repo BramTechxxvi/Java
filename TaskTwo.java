4b714f8be932fa75c67c696a8dca052b11b71779
BramTechxxvi <bramtechxxvi@gmail.com>
2024-12-07 11:33:40 +0100

    Add files via upload

---
+++ TaskTwo.java
@@ -0,0 +1,23 @@
+public class TaskTwo{
+	public static void main(String[] args){
+
+
+		System.out.println();
+	for(int number = 1; number <= 10; number++){
+	if(number % 2 == 0){
+	System.out.println(number);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+}
+}
