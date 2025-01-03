2e81c79c700ae0d2fa3fa71fb2b255ac1087e542
BramTechxxvi <bramtechxxvi@gmail.com>
2025-01-03 11:51:02 +0100

    new changes

---
+++ TaskSeven.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TaskSeven{
+
+	public static void main(String[] args){
+	Scanner userCollector = new Scanner(System.in);  
+                                                                                                                                                                             
+int counter = 4;
+int answer = 1;
+
+	for (int number = 1; number <= 5; number++){
+	             answer*=counter; 
+		     System.out.println(answer);
+}	
+	int count = 8;
+	int result = 1;
+		
+		for(int number = 1; number <= 5; number++){
+			result*=count;
+		    System.out.println(result);
+
+
+
+}
+}
+
+}
+
---
+++ TaskSix.java
@@ -0,0 +1,36 @@
+public class TaskSix{
+		public static void main(String[] args){
+
+
+
+
+	int counter = 4;
+	int answer1 = 1;
+		for(int number = 1; number <= 5; number++){
+	 	  answer1*=counter;
+	  	 System.out.println(answer1);
+
+
+
+
+}
+
+	int count = 8;
+	int answer2 = 1;
+		for(int number = 1; number <= 5; number++){
+	 	  answer2*=count;
+         	  System.out.println(answer2);
+	
+	
+}
+	
+
+
+
+
+}
+
+
+
+
+}
