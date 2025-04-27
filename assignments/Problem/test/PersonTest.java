8063f6b092024a60ea773db36bf220db9d5148c7
BramTechxxvi <bramtechxxvi@gmail.com>
2025-04-27 17:14:18 +0100

    new commit

--- assignments/Problem/test/PersonTest.java
+++ assignments/Problem/test/PersonTest.java
@@ -18,15 +18,15 @@ class PersonTest {
 
     @Test
     public void test_If_Person_Can_Add_Problem__addProblem() {
-        Problem problem = new Problem("Wierd dreams", ProblemType.SPIRITUAL);
+        Problem problem = new Problem("Dream", "Wierd dreams", ProblemType.SPIRITUAL);
         newPerson.addProblem(problem);
         assertEquals(1, newPerson.getSize());
     }
 
     @Test
     public void test_If_Person_Can_Add_Multiple_Problems__addProblem() {
-        Problem problem1 = new Problem("Wierd dreams", ProblemType.SPIRITUAL);
-        Problem problem2 = new Problem("I can't write", ProblemType.EDUCATIONAL);
+        Problem problem1 = new Problem("Dream", "Wierd dreams", ProblemType.SPIRITUAL);
+        Problem problem2 = new Problem("Illiteracy", "I can't write", ProblemType.EDUCATIONAL);
         newPerson.addProblem(problem1);
         newPerson.addProblem(problem2);
         assertEquals(2, newPerson.getSize());
@@ -34,8 +34,8 @@ class PersonTest {
 
     @Test
     public void test_If_Person_Can_Solve_Problem__solve() {
-        Problem problem1 = new Problem("Wierd dreams", ProblemType.SPIRITUAL);
-        Problem problem2 = new Problem("I can't write", ProblemType.EDUCATIONAL);
+        Problem problem1 = new Problem("Dream", "Wierd dreams", ProblemType.SPIRITUAL);
+        Problem problem2 = new Problem("Illiteracy", "I can't write", ProblemType.EDUCATIONAL);
         newPerson.addProblem(problem1);
         newPerson.addProblem(problem2);
         newPerson.solveProblem(problem1);
