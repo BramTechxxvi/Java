74dd6da01af0493c06dfcb5b543abbe5681f0390
BramTechxxvi <bramtechxxvi@gmail.com>
2025-05-07 12:29:23 +0100

    new commit

---
+++ proTest/TurtleGraphicsTest.java
@@ -0,0 +1,6 @@
+package proTest;
+import pro.TurtleGraphics;
+
+public class TurtleGraphicsTest {
+    TurtleGraphics lola = new TurtleGraphics();
+}
--- protest/TurtleGraphicsTest.java
+++
@@ -1,9 +0,0 @@
-package protest;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import pro.TurtleGraphics;
-
-public class TurtleGraphicsTest {
-    TurtleGraphics lola = new TurtleGraphics();
-}
