5c1ca4cf99b5b1798038de9841f6175e4a503fe3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-05-17 14:50:11 +0100

    new commit

---
+++ QuickTeaDays.java
@@ -0,0 +1,18 @@
+public enum QuickTeaDays {
+    MONDAY("Monday"),
+    TUESDAY("Tuesday"),
+    WEDNESDAY("Wednesday"),
+    THURSDAY("Thursday"),
+    FRIDAY("Friday"),
+    SATURDAY("Saturday"),
+    SUNDAY("Sunday");
+
+    private String day;
+    QuickTeaDays(String day) {
+        this.day = day;
+    }
+
+
+}
+
+
---
+++ QuickTeaTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class QuickTeaTest {
+
+    private QuickTea quickTea;
+    @BeforeEach
+    void setUp() {
+        quickTea = new QuickTea();
+    }
+
+    @Test
+    public void test() {
+        quickTea = new QuickTea();
+
+    }
+
+}
