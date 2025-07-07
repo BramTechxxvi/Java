2e2abba44021c2c28588081be365fca9bf8d5cd8
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-07 10:45:24 +0100

    first commit

---
+++ chapter15/test/chapter15/FilesOperationsTest.java
@@ -0,0 +1,16 @@
+package chapter15;
+
+import org.junit.jupiter.api.Test;
+
+public class FilesOperationsTest {
+
+    @Test
+    void testCabnReadFromFile() {
+        String fileLocation = "";
+        String data - FileOperations.readFrom(fileLocation);
+    }
+
+
+
+
+}
