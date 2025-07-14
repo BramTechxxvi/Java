a49e40a93d43cb2ee666eae59ee77937fa30e470
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-14 16:50:37 +0100

    new

--- saveToFile/src/TransactionDateDeserializer.java
+++ saveToFile/src/TransactionDateDeserializer.java
@@ -1,2 +1,15 @@
-public class TransactionDateDeserializer {
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class TransactionDateDeserializer extends JsonDeserializer<LocalDate> {
+
+    @Override
+    public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        return null;
+    }
 }
