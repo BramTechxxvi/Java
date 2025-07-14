a33ace2276849c8b8ff3237b170a2226705da1bc
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-14 16:28:43 +0100

    new

--- saveToFile/src/TransactionJsonSerializer.java
+++ saveToFile/src/TransactionJsonSerializer.java
@@ -1,16 +1,23 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+
 public class TransactionJsonSerializer {
 
     public static String serialize(Transaction transaction) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
             String json = mapper.writeValueAsString(transaction);
             System.out.println("Json:: " + json);
             return json;
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         } return " ";
+    }
+
+    public static String serializeTransactions(List<Transaction> transactionList) {
+        try {
+            return
+        }
     }
 }
