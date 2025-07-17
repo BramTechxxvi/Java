c262c0f7ac08faeb4f4bffdd756639da2727b766
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-17 12:30:17 +0100

    new

--- MorningTea/src/Lcm.java
+++ MorningTea/src/Lcm.java
@@ -1,2 +1,14 @@
 public class Lcm {
+
+    public int greatestCommonDivisor(int... numbers) {
+        if(numbers.length == 0) throw new IllegalArgumentException("You must provide at least one number");
+
+        return 0;
+    }
+
+    public int gcd(int a, int b) {
+//        if(b == 0) return a;
+//        return gcd(b, a % b);
+
+    }
 }
--- MorningTea/test/LcmTest.java
+++ MorningTea/test/LcmTest.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class LcmTest {
-  
+
+    @Test
+    void testCanFindGcf__greatestCommonFsctor() {
+        Lcm lcm = new Lcm();
+        int result = lcm.greatestCommonDivisor(1, 4, 8, 20, 29);
+        assertEquals(1, result);
+    }
 }
