660a217d87faf48a47f014e8ed38e42a3909874d
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-17 12:36:03 +0100

    new

--- MorningTea/src/Lcm.java
+++ MorningTea/src/Lcm.java
@@ -2,13 +2,18 @@ public class Lcm {
 
     public int greatestCommonDivisor(int... numbers) {
         if(numbers.length == 0) throw new IllegalArgumentException("You must provide at least one number");
-
+        for (int count = 0; count < numbers.length; count++) {
+            result = gcd(result, numbers[i])
+        }
         return 0;
     }
 
-    public int gcd(int a, int b) {
-//        if(b == 0) return a;
-//        return gcd(b, a % b);
-
+    public int gcd(int firstNumber, int secondNumber) {
+        while (secondNumber != 0) {
+            int temp = secondNumber;
+            secondNumber = firstNumber % secondNumber;
+            firstNumber = temp;
+        }
+        return firstNumber;
     }
 }
