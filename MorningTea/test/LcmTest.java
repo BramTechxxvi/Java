242fe595d4fc6e5abf80f066a5c112e4f92cf281
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-17 17:14:08 +0100

    new

--- MorningTea/test/LcmTest.java
+++ MorningTea/test/LcmTest.java
@@ -6,7 +6,8 @@ class LcmTest {
     @Test
     void testCanFindGcf__greatestCommonFsctor() {
         Lcm lcm = new Lcm();
-        int result = lcm.greatestCommonDivisor(1, 4, 8, 20, 29);
-        assertEquals(1, result);
+        assertEquals(1, lcm.greatestCommonDivisor(1, 4, 8, 20, 29));
+        assertEquals(4, lcm.greatestCommonDivisor(4, 8));
+        assertEquals(9, lcm.greatestCommonDivisor(27, 81, 18));
     }
 }
