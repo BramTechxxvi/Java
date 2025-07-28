34d88092cf4bb9eacf0c22ad2a29d6a74bb730a8
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-28 15:16:06 +0100

    new

--- chapter17/src/Car.java
+++ chapter17/src/Car.java
@@ -1,2 +1,7 @@
-public class Car {
+public class Car implements Movable{
+
+    @Override
+    public void move(int distance) {
+        System.out.println("Bicycle moved: " + distance + " miles");
+    }
 }
--- chapter17/src/Movable.java
+++ chapter17/src/Movable.java
@@ -1,4 +1,4 @@
 public interface Movable {
 
-    void move();
+    void move(int distance);
 }
