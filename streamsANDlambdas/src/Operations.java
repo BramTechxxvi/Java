2c45ec9259ea478b43f952fcfcf536d8398c8753
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-28 12:33:09 +0100

    new

---
+++ streamsANDlambdas/src/Operations.java
@@ -0,0 +1,27 @@
+public class Operations {
+
+    public static addNumbers() {
+
+    }
+    public static subtract() {
+
+    }
+
+    public static divide() {
+
+    }
+
+    public static multiply() {
+
+    }
+
+    public static evenNumbers() {
+
+    }
+
+    public static oddNumbers() {
+
+    }
+
+
+}
