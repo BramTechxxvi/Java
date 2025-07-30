0daee9b3eff48e262d6b4bd274a45301f388dabf
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-30 16:04:08 +0100

    first commit

--- chapter17/src/taskTwo/FunctionImpl.java
+++ chapter17/src/taskTwo/FunctionImpl.java
@@ -1,8 +1,12 @@
 package taskTwo;
 
+import java.util.function.Function;
+
 public class FunctionImpl {
 
     public static void main(String[] args) {
 
+        Function<String, Integer> getLength = (text)-> text.length();
+        System.out.println(getLength.apply("Hello"));
     }
 }
