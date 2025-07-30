04f0a7964cb65b2d4313e88644c0ac8fae973749
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-30 16:08:17 +0100

    first commit

--- chapter17/src/taskTwo/FunctionImpl.java
+++ chapter17/src/taskTwo/FunctionImpl.java
@@ -8,5 +8,13 @@ public class FunctionImpl {
 
         Function<String, Integer> getLength = (text)-> text.length();
         System.out.println(getLength.apply("Hello"));
+
+        Function<String, Boolean> booleanFunction = (text)->
+                text.matches("[0-9]+");
+
+        boolean result = booleanFunction.apply("Hello");
+        System.out.println(result);
+        boolean result2 = booleanFunction.apply("Hello123");
+        System.out.println(result2);
     }
 }
