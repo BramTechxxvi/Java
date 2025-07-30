1331a63df174d14ad4237a2a6b218d6ea7669982
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-30 15:25:38 +0100

    first commit

--- chapter17/src/taskTwo/BiConsumerImpl.java
+++ chapter17/src/taskTwo/BiConsumerImpl.java
@@ -1,4 +1,12 @@
 package taskTwo;
 
+import java.util.function.BiConsumer;
+
 public class BiConsumerImpl {
+
+    public static void main(String[] args) {
+        BiConsumer<Integer, Integer> biConsumer = ()-> {
+
+        };
+    }
 }
