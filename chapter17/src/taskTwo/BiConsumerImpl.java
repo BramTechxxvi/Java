d25de51c316dbe101bbdef5893fc5ccfcd17099b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-30 15:31:05 +0100

    first commit

--- chapter17/src/taskTwo/BiConsumerImpl.java
+++ chapter17/src/taskTwo/BiConsumerImpl.java
@@ -1,12 +1,19 @@
 package taskTwo;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
 public class BiConsumerImpl {
 
     public static void main(String[] args) {
-        BiConsumer<Integer, Integer> biConsumer = ()-> {
+        BiConsumer<Integer, Integer> biConsumer = (a,b)-> System.out.println(a * b);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1,5);
+        map.put(2,3);
+        map.put(30,4);
+        map.put(40,5);
 
-        };
+        map.forEach(biConsumer);
     }
 }
