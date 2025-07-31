4359f66af23a563cd549d70d969421503f539d74
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 16:22:07 +0100

    new

--- chapter17/src/taskThree/StreamLimiter.java
+++ chapter17/src/taskThree/StreamLimiter.java
@@ -1,8 +1,13 @@
 package taskThree;
 
+import java.util.Random;
+import java.util.stream.Stream;
+
 public class StreamLimiter {
 
     public static void main(String[] args) {
-
+        Stream.generate(()->new Random().nextInt(10))
+                .limit(10)
+                .forEach(System.out::println);
     }
 }
