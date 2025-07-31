9e930dc366e8745c1fd012df167ded4691683edf
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 16:28:10 +0100

    new

--- chapter17/src/taskThree/MapStreamer.java
+++ chapter17/src/taskThree/MapStreamer.java
@@ -1,4 +1,13 @@
 package taskThree;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MapStreamer {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.stream().filter((num) -> num % 2 == 0).map((num) -> num * num)
+                .forEach(System.out::println);
+    }
 }
