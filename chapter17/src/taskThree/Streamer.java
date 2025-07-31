2fcf629728398e97797ef50c87e540e248c764a3
BramTechxxvi <bramtechxxvi@gmail.com>
2025-07-31 16:17:36 +0100

    new

--- chapter17/src/taskThree/Streamer.java
+++ chapter17/src/taskThree/Streamer.java
@@ -1,8 +1,16 @@
 package taskThree;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Streamer {
 
     public static void main(String[] args) {
 
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Stream<Integer> integerStream = numbers.stream();
+
+        integerStream.filter((num -> ))
     }
 }
