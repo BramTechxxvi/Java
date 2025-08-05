acd117aca9f356d30602843dc376cdc4cd93d876
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-05 05:05:17 +0100

    new

--- chapter17/src/taskTwo/BiFunctionImpl.java
+++ chapter17/src/taskTwo/BiFunctionImpl.java
@@ -1,6 +1,7 @@
 package taskTwo;
 
 import java.util.function.BiFunction;
+import java.util.stream.IntStream;
 
 public class BiFunctionImpl {
 
@@ -8,8 +9,12 @@ public class BiFunctionImpl {
 
         BiFunction<String, Integer, Boolean> isCorrect = (text, num)-> text.length() == num;
         System.out.println(isCorrect.apply("Adedeji", 9));
-
-        BiFunction<>
+//
+//        BiFunction<Integer, Integer, String> print = (numTol, number)->{
+//            IntStream.rangeClosed(1,numTol).filter(num -> %2 == 0)
+//
+//                    .forEach();
+//        };
 
     }
 }
