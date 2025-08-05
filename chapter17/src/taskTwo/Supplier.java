5a50ca1b6827760222399c01b77a93d52f8b3e3c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-05 04:23:07 +0100

    new

--- chapter17/src/taskTwo/Supplier.java
+++
@@ -1,8 +0,0 @@
-package taskTwo;
-
-public class Supplier {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ chapter17/src/taskTwo/SupplierImpl.java
@@ -0,0 +1,12 @@
+package taskTwo;
+
+import java.util.function.Supplier;
+
+public class SupplierImpl {
+
+    public static void main(String[] args) {
+
+        Supplier<String>
+
+    }
+}
