a3b9238f276bacb1b8a7f0e21c3b8fdd65449959
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-05 04:41:07 +0100

    new

--- chapter17/src/taskTwo/BiFunctionImpl.java
+++ chapter17/src/taskTwo/BiFunctionImpl.java
@@ -1,8 +1,16 @@
 package taskTwo;
 
+import java.util.function.BiFunction;
+
 public class BiFunctionImpl {
 
     public static void main(String[] args) {
+
+        BiFunction<String, Integer, Boolean> isCorrect = (text, num)->{
+            return text.length() == num;
+        };
+
+        System.out.println(isCorrect.apply("Adedeji", 9));
 
     }
 }
