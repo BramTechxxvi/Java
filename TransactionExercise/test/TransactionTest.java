df9fc6dd9e5a3015ca8088f4e0754250cf05c859
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-06 11:34:01 +0100

    new

--- TransactionExercise/test/TransactionTest.java
+++ TransactionExercise/test/TransactionTest.java
@@ -1,10 +1,19 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
     @Test
     public void testThatTr() {
+        List<Transaction> transactions = new ArrayList<Transaction>();
         Transaction firstTransaction = new Transaction();
+        firstTransaction.setSender("John Adah");
+        firstTransaction.setReceiver("Matthew");
+        firstTransaction.setAmount(10_000);
+        firstTransaction.setDate(LocalDate.now());
     }
 
 }
