9f4f6adbe407972318bce551368df0d3ca0f5d60
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-06 11:31:47 +0100

    new

--- TransactionExercise/src/Transaction.java
+++ TransactionExercise/src/Transaction.java
@@ -19,6 +19,10 @@ public class Transaction {
 
     public void setSender(String sender) { this.sender = sender; }
 
+    public String getReceiver() { return receiver; }
+
+    public void setReceiver(String receiver) { this.receiver = receiver; }
+
 
 
 }
