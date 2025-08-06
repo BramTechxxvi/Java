f220b04dcde257b1a914cf2410a6a07ed64c395b
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-06 11:30:32 +0100

    new

--- TransactionExercise/src/Transaction.java
+++ TransactionExercise/src/Transaction.java
@@ -1,4 +1,14 @@
+import java.time.LocalDate;
+
 public class Transaction {
 
-    private
+    private LocalDate date;
+    private int amount;
+    private String sender;
+    private String receiver;
+
+    public LocalDate getDate() { return date; }
+
+
+
 }
--- TransactionExercise/test/TransactionTest.java
+++ TransactionExercise/test/TransactionTest.java
@@ -2,4 +2,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
 
+    @Test
+    public void testThatTr() {
+        Transaction firstTransaction = new Transaction();
+    }
+
 }
