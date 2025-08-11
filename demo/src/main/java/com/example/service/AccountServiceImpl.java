f51559e7239eff99821b500e4f300baff333b2af
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 10:15:41 +0100

    new

--- demo/src/main/java/com/example/service/AccountServiceImpl.java
+++ demo/src/main/java/com/example/service/AccountServiceImpl.java
@@ -1,4 +1,12 @@
 package com.example.service;
 
-public class AccountServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+    @Override
+    public String createAccount(String phoneNumber) {
+        return "Account created";
+    }
 }
