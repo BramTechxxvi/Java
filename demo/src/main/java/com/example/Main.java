83ed7ef0479451d07be4403e20e2234fe673f72f
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-11 10:25:45 +0100

    new

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,9 +1,23 @@
 package com.example;
 
+import com.example.config.ProjectConfig;
+import com.example.controller.AccountController;
+import com.example.service.AccountService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext appContext = new AnnotationConfigApplicationContext(projectConfig.class)
-        
-        System.out.println("Hello world!");
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        AccountService acctService = appContext.getBean(AccountService.class);
+        System.out.println(appContext);
+        System.out.println("accountService 1: " + acctService);
+
+        AccountService acctService2 = appContext.getBean(AccountService.class);
+        System.out.println("accountService 2: " + acctService2);
+
+        AccountController acctController = appContext.getBean(AccountController.class);
+        System.out.println("accountController: " + acctController);
+
     }
 }
